0caa6750f8833519aff7d8c201c33422a1e5fb8f
Huskago <tiagoflfl0202@gmail.com>
2023-06-23 04:18:19 +0200

    Update background

--- src/components/SpaceBackground.tsx
+++ src/components/SpaceBackground.tsx
@@ -1,9 +1,81 @@
-import { SpaceBackgroundAnimation } from './utils/SpaceBackgroundAnimation.tsx';
+import React, { useState, useEffect } from 'react';
 
-export const SpaceBackground = () => {
+interface SpaceBackgroundProps {
+    starSpeed: number;
+    starSize: number;
+}
+
+interface Star {
+    left: number;
+    top: number;
+}
+
+export const SpaceBackground: React.FC<SpaceBackgroundProps> = ({ starSpeed, starSize }) => {
+    const [stars, setStars] = useState<Star[]>([]);
+
+    useEffect(() => {
+        const updateStars = () => {
+            setStars(prevStars => {
+                const updatedStars = prevStars.map(star => ({
+                    left: star.left - starSpeed,
+                    top: star.top,
+                })).filter(star  => star.left + starSize > 0);
+
+                const numNewStars = Math.floor(window.innerWidth / 50) - updatedStars.length;
+                const newStars = Array.from({ length: numNewStars }, () => ({
+                    left: window.innerWidth,
+                    top: Math.floor(Math.random() * window.innerHeight),
+                }));
+
+                return [...updatedStars, ...newStars];
+            });
+        };
+
+        const generateStars = () => {
+            const numStars = Math.floor(window.innerWidth / 50);
+
+            const newStars = Array.from({ length: numStars }, () => ({
+                left: Math.floor(Math.random() * window.innerWidth),
+                top: Math.floor(Math.random() * window.innerHeight),
+            }));
+
+            setStars(newStars);
+        };
+
+        generateStars();
+        const interval = setInterval(updateStars, 50);
+
+        return () => clearInterval(interval);
+    }, [starSize, starSpeed]);
+
+    useEffect(() => {
+        console.log(stars.length)
+    }, [stars.length]);
+
     return (
-        <div className={"fixed top-0 left-0 w-full h-full bg-black overflow-hidden"}>
-            <SpaceBackgroundAnimation />
+        <div
+            style={{
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                backgroundColor: 'black',
+            }}
+        >
+            {stars.map((star, index) => (
+                <div
+                    key={index}
+                    style={{
+                        position: 'absolute',
+                        left: star.left,
+                        top: star.top,
+                        width: starSize,
+                        height: starSize,
+                        backgroundColor: 'white',
+                    }}
+                />
+            ))}
         </div>
     );
 };
