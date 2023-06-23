184e7828ecdc1d91a13578bd7805d7307425ba3e
Huskago <tiagoflfl0202@gmail.com>
2023-06-23 06:18:33 +0200

    Update

--- src/utils/GenerateSheet.tsx
+++ src/utils/GenerateSheet.tsx
@@ -1,6 +1,33 @@
 import { createCanvas, loadImage } from "canvas";
 import { Character } from "../components/CreateSheet.tsx";
 
+const wrapText = function(ctx, text, x, y, maxWidth, lineHeight) {
+    let words = text.split(" ")
+    let line = ""
+    let testLine = ""
+    let lineArray= []
+
+    for (var n = 0; n < words.length; n++) {
+        testLine += `${words[n]} `
+        let metrics = ctx.measureText(testLine)
+        let testWidth = metrics.width
+        if (testWidth > maxWidth && n > 0) {
+            lineArray.push([line, x, y])
+            y += lineHeight
+            line = `${words[n]} `
+            testLine = `${words[n]} `
+        }
+        else {
+            line += `${words[n]} `
+        }
+        if (n === words.length - 1) {
+            lineArray.push([line, x, y])
+        }
+    }
+
+    return lineArray
+}
+
 export const generatePNG = async (character: Character) => {
     const career = character.career?.getName() ?? ""
     const attributs = {
@@ -25,9 +52,16 @@ export const generatePNG = async (character: Character) => {
         medicalAid: character.skills?.medicalAid.getValue().toString() ?? "0",
     }
 
-    const talent = character.talent?.getName() ?? ""
+    const talent = character.talent?.name ?? ""
+
+    const equipment1 = character.equipments?.[0]?.name ?? ""
+    const equipment2 = character.equipments?.[1]?.name ?? ""
 
     const name = character.personalInfo?.name ?? ""
+    const objective = character.personalInfo?.objective ?? ""
+    const buddy = character.personalInfo?.buddy ?? ""
+    const rival = character.personalInfo?.rival ?? ""
+    const appearance = character.personalInfo?.appearance ?? ""
 
     try {
         const image = await loadImage("/sheets/fiche_complete.png")
@@ -151,6 +185,21 @@ export const generatePNG = async (character: Character) => {
         ctx.textAlign = "left"
         ctx.fillText(talent, canvas.width - (canvas.width / 4 + 138), 190)
 
+        // Equipment
+        if (character.equipments) {
+            if (character.equipments.length === 2) {
+                ctx.font = "bold 26px OCR A Std Regular"
+                ctx.fillStyle = "black"
+                ctx.textAlign = "left"
+                ctx.fillText(equipment1, canvas.width - (canvas.width / 4 + 50), canvas.height - canvas.height / 3 + 8)
+            }
+
+            ctx.font = "bold 26px OCR A Std Regular"
+            ctx.fillStyle = "black"
+            ctx.textAlign = "left"
+            ctx.fillText(equipment2, canvas.width - (canvas.width / 4 + 50), canvas.height - canvas.height / 3 - 40)
+        }
+
 
         // Name
         ctx.font = "bold 26px OCR A Std Regular"
@@ -158,6 +207,37 @@ export const generatePNG = async (character: Character) => {
         ctx.textAlign = "center"
         ctx.fillText(name, canvas.width/2, 200)
 
+        // Objective
+        const wrappedTextObjective = wrapText(ctx, objective, 130, 170, 1000, 20)
+
+        wrappedTextObjective.forEach(function(item) {
+            ctx.font = "bold 16px OCR A Std Regular"
+            ctx.fillStyle = "black"
+            ctx.textAlign = "left"
+            ctx.fillText(item[0], item[1], item[2])
+        })
+
+        // Rival
+        ctx.font = "bold 24px OCR A Std Regular"
+        ctx.fillStyle = "black"
+        ctx.textAlign = "left"
+        ctx.fillText(rival, 280, 350)
+
+        // Buddy
+        ctx.font = "bold 24px OCR A Std Regular"
+        ctx.fillStyle = "black"
+        ctx.textAlign = "left"
+        ctx.fillText(buddy, 220, 405)
+
+        // Appearance
+        const wrappedTextAppearance = wrapText(ctx, appearance, canvas.width/2, 365, 800, 20)
+
+        wrappedTextAppearance.forEach(function(item) {
+            ctx.font = "bold 20px OCR A Std Regular"
+            ctx.fillStyle = "black"
+            ctx.textAlign = "center"
+            ctx.fillText(item[0], item[1], item[2])
+        })
 
         return canvas.toDataURL("image/png");
     } catch (error) {
